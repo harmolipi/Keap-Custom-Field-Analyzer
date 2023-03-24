a498c9e105780c2003b098ed472657747601bfc7
Niko <nikob381@gmail.com>
2023-03-24 02:38:40 -0400

    refactor(custom fields): move custom field retrieval into separate function

--- public/index.php
+++ public/index.php
@@ -41,27 +41,7 @@ if (isset($_GET['code']) and !$infusionsoft->getToken()) {
 if ($infusionsoft->getToken()) {
   $_SESSION['token'] = serialize($infusionsoft->getToken());
 
-  $customFieldService = $infusionsoft->customFields();
-
-  $dataService = $infusionsoft->data();
-  $query = array('FormId' => -1);
-  $select = array('Id', 'Label', 'Name', 'Values', 'FormId');
-  $orderBy = 'Label';
-  $limit = 1000;
-  $page = 0;
-  $customFields = array();
-  $ascending = true;
-
-  do {
-    $results = $dataService->query('DataFormField', $limit, $page, $query, $select, $orderBy, $ascending);
-    $customFields = array_merge($customFields, $results);
-    $page++;
-  } while (count($results) == $limit);
-
-  // Set each custom field's key as its id:
-  $custom_field_ids = array_column($customFields, 'Id');
-  $customFieldsArray = new ArrayObject($customFields);
-  $customFields = array_combine($custom_field_ids, $customFields);
+  $customFields = getCustomFields($infusionsoft);
 
   $offset = 0;
   $limit = 1000;
@@ -146,4 +126,31 @@ if ($infusionsoft->getToken()) {
   d($custom_field_counts_by_name);
 } else {
   echo '<a rel="nofollow" href="' . $infusionsoft->getAuthorizationUrl() . '">Click here to authorize</a>';
+}
+
+function getCustomFields(Infusionsoft $infusionsoft): array
+{
+  $customFieldService = $infusionsoft->customFields();
+
+  $dataService = $infusionsoft->data();
+  $query = array('FormId' => -1);
+  $select = array('Id', 'Label', 'Name', 'Values', 'FormId');
+  $orderBy = 'Label';
+  $limit = 1000;
+  $page = 0;
+  $customFields = array();
+  $ascending = true;
+
+  do {
+    $results = $dataService->query('DataFormField', $limit, $page, $query, $select, $orderBy, $ascending);
+    $customFields = array_merge($customFields, $results);
+    $page++;
+  } while (count($results) == $limit);
+
+  // Set each custom field's key as its id:
+  $custom_field_ids = array_column($customFields, 'Id');
+  $customFieldsArray = new ArrayObject($customFields);
+  $customFields = array_combine($custom_field_ids, $customFields);
+
+  return $customFields;
 }
